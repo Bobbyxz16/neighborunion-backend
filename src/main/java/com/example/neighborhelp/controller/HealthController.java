8df9e9b5fda0a32014147730947646a5113f6ef4
Bobbyxz16 <bobbyokolie14@gmail.com>
2025-10-07 13:08:56 +0100

    Healthcrotroller included

---
+++ src/main/java/com/example/neighborhelp/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.example.neighborhelp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
